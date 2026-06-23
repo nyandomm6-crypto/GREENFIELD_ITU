4866985ec504be7ea4c96937663d2e1b81ed3d8f
MAMINIAVO Grace Tanteraka <gracetanteraka@gmail.com>
2026-06-23 21:25:47 +0300

    [update] Add public modifier to findByMail method in ClientRepository

--- src/main/java/itu/GreenField/repository/ClientRepository.java
+++ src/main/java/itu/GreenField/repository/ClientRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import itu.GreenField.model.Client;
 
 public interface ClientRepository extends JpaRepository<Client, Integer> {
-    Client findByMail(String email);
+    public Client findByMail(String email);
+    
 }
