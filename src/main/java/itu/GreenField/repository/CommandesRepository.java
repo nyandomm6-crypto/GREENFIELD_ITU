b762a0c1c88e648495995f3ad9393927be0a6d5e
MAMINIAVO Grace Tanteraka <gracetanteraka@gmail.com>
2026-06-23 21:25:56 +0300

    [add] Implement findDispoCommandes method in CommandesRepository to retrieve available commandes

--- src/main/java/itu/GreenField/repository/CommandesRepository.java
+++ src/main/java/itu/GreenField/repository/CommandesRepository.java
@@ -1,9 +1,17 @@
 package itu.GreenField.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import itu.GreenField.model.Commandes;
+import java.util.List;
 
 public interface CommandesRepository extends JpaRepository<Commandes, Integer> {
     public Commandes getById(Integer id);
+
+    @Query(value = "SELECT * FROM commandes c " +
+            "WHERE c.statutcommande != 'Paye' " +
+            "AND c.statutcommande != 'Annule'", nativeQuery = true)
+    List<Commandes> findDispoCommandes();
 }
