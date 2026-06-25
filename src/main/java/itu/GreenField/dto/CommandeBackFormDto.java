ec703e2e8b51a498dc9012857268f30783c286d3
MAMINIAVO Grace Tanteraka <gracetanteraka@gmail.com>
2026-06-25 19:17:42 +0300

    [refactor] change name of CommandeFormDto to CommandeBackFormDto

--- src/main/java/itu/GreenField/dto/CommandeBackFormDto.java
+++ src/main/java/itu/GreenField/dto/CommandeBackFormDto.java
@@ -5,7 +5,7 @@ import java.time.LocalTime;
 import java.util.List;
 import org.springframework.format.annotation.DateTimeFormat;
 
-public class CommandeFormDto {
+public class CommandeBackFormDto {
     // Client
     private Integer clientId;
     private String clientNom;
@@ -23,10 +23,32 @@ public class CommandeFormDto {
     @DateTimeFormat(pattern = "HH:mm")
     private LocalTime heureReceptionFin;
 
+    private String address;
+
     // Details
     private List<String> produitMatricule;
     private List<Integer> qte;
 
+    public java.sql.Timestamp getSqlTypeOfDate() {
+        if (date != null) {
+            return java.sql.Timestamp.valueOf(date);
+        }
+        return null;
+    }
+
+    public java.sql.Time getSqlTypeOfHeureReceptionDebut() {
+        if (heureReceptionDebut != null) {
+            return java.sql.Time.valueOf(heureReceptionDebut);
+        }
+        return null;
+    }
+
+    public java.sql.Time getSqlTypeOfHeureReceptionFin() {
+        if (heureReceptionFin != null) {
+            return java.sql.Time.valueOf(heureReceptionFin);
+        }
+        return null;
+    }
 
     public Integer getClientId() {
         return clientId;
@@ -98,6 +120,14 @@ public class CommandeFormDto {
 
     public void setQte(List<Integer> qte) {
         this.qte = qte;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
 }
