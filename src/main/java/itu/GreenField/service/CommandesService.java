11d5734ce6fca0923989425fc3801a37444b031c
MAMINIAVO Grace Tanteraka <gracetanteraka@gmail.com>
2026-06-25 19:24:13 +0300

    [update] implementing full transactionnal methode saveBackCommande

--- src/main/java/itu/GreenField/service/CommandesService.java
+++ src/main/java/itu/GreenField/service/CommandesService.java
@@ -1,22 +1,37 @@
 package itu.GreenField.service;
 
+import itu.GreenField.service.ProduitService;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 import jakarta.transaction.Transactional;
-import itu.GreenField.dto.CommandeFormDto;
+import itu.GreenField.dto.CommandeBackFormDto;
 import itu.GreenField.model.Client;
 import itu.GreenField.model.Commandes;
+import itu.GreenField.model.ModeReception;
+import itu.GreenField.model.PointDeVente;
+import itu.GreenField.model.Produit;
 import itu.GreenField.repository.CommandesRepository;
+import itu.GreenField.repository.DetailsCommandeRepository;
+import itu.GreenField.model.StatutCommande;
+import itu.GreenField.model.DetailsCommande;
 
 @Service
 public class CommandesService {
+    private final ProduitService produitService;
     private final CommandesRepository commandesRepository;
     private final ClientService clientService;
+    private final PointDeVenteService pointDeVenteService;
+    private final DetailsCommandeRepository detailsCommandeRepository;
 
-    public CommandesService(CommandesRepository commandesRepository, ClientService clientService) {
+    public CommandesService(ProduitService produitService, CommandesRepository commandesRepository,
+            ClientService clientService, PointDeVenteService pointDeVenteService,
+            DetailsCommandeRepository detailsCommandeRepository) {
+        this.produitService = produitService;
         this.commandesRepository = commandesRepository;
         this.clientService = clientService;
+        this.pointDeVenteService = pointDeVenteService;
+        this.detailsCommandeRepository = detailsCommandeRepository;
     }
 
     public List<Commandes> getCommandesDispo() {
@@ -28,7 +43,7 @@ public class CommandesService {
     }
 
     @Transactional
-    public void saveCommande(CommandeFormDto commandeFormDto) {
+    public void saveBackCommande(CommandeBackFormDto commandeFormDto) {
         Integer clientId = commandeFormDto.getClientId();
         Client client = null;
         if (clientId != null) {
@@ -40,8 +55,48 @@ public class CommandesService {
             client = clientService.saveClient(client);
         }
 
+        ModeReception modeReception = ModeReception.fromString(commandeFormDto.getModeReception());
+        StatutCommande statusCommande = StatutCommande.En_cours;
+
         Commandes commande = new Commandes();
         commande.setClient(client);
+        commande.setDatecommande(commandeFormDto.getSqlTypeOfDate());
+        commande.setModeReception(modeReception);
+        commande.setHeureReceptionDebut(commandeFormDto.getSqlTypeOfHeureReceptionDebut());
+        commande.setHeureReceptionFin(commandeFormDto.getSqlTypeOfHeureReceptionFin());
+        commande.setAdresseLivraison(commandeFormDto.getAddress());
+        commande.setStatutCommande(statusCommande);
+
+        /* Static pour le moment */
+        if(commandeFormDto.getAddress() == null || modeReception == ModeReception.Retrait_Boutique){
+            PointDeVente pdv = pointDeVenteService.findPointDeVenteById(1);
+            commande.setPointDeVenteRetrait(pdv);
+        }
+
+        commande.setFraisLivraison(5000.0);
+        commande = commandesRepository.save(commande);
+
+        int nbLines = commandeFormDto.getProduitMatricule().size();
+        int qteTotal = 0;
+        double prixTotal = 0.0;
+        for (int i = 0; i < nbLines; i++) {
+            String matricule = commandeFormDto.getProduitMatricule().get(i);
+            Integer quantite = commandeFormDto.getQte().get(i);
+            DetailsCommande detail = new DetailsCommande();
+            Produit produit = produitService.findProduitByMatricule(matricule);
+            detail.setCommande(commande);
+            detail.setProduit(produit);
+            detail.setQuantite(quantite);
+            detail.setPuAuMomentAchat(produit.getPu());
+            detailsCommandeRepository.save(detail);
+
+            qteTotal += quantite;
+            prixTotal += ((double) quantite) * produit.getPu();
+        }
+
+        commande.setTotalProduits(qteTotal);
+        commande.setTotalGeneral(prixTotal);
+
         commandesRepository.save(commande);
     }
 
