6d456f9eef5e4df89bc020fcb3d299cd89d4412d
Yrielle <yrielledarvisenet@gmail.com>
2026-06-27 13:01:54 +0300

    module-3-vehicules

--- src/main/java/itu/GreenField/repository/VehiculeRepository.java
+++ src/main/java/itu/GreenField/repository/VehiculeRepository.java
@@ -1,8 +1,25 @@
 package itu.GreenField.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import itu.GreenField.model.StatutVehicule;
 import itu.GreenField.model.Vehicule;
 
 public interface VehiculeRepository extends JpaRepository<Vehicule, Integer> {
+    // II-Liste vehicule : Filtre par mot clés, date, status
+    @Query("SELECT v FROM Vehicule v WHERE " +
+           "(:motCle IS NULL OR LOWER(v.marque) LIKE LOWER(CONCAT('%', :motCle, '%')) " +
+           "OR LOWER(v.modele) LIKE LOWER(CONCAT('%', :motCle, '%')) " +
+           "OR LOWER(v.matricule) LIKE LOWER(CONCAT('%', :motCle, '%'))) " +
+           "AND (:date IS NULL OR v.date = :date) " +
+           "AND (:statut IS NULL OR v.statut = :statut)")
+    List<Vehicule> filtrerVehicules(@Param("motCle") String motCle, 
+                                    @Param("date") LocalDate date, 
+                                    @Param("statut") StatutVehicule statut);
+
 }
