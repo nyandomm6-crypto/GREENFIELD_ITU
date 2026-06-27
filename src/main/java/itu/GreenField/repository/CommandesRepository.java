b767635fd0882fb61e858984763882fc1b1d141e
MAMINIAVO Grace Tanteraka <gracetanteraka@gmail.com>
2026-06-27 12:01:17 +0300

    [feat] update CommandesRepository to include pagination and filtering methods

--- src/main/java/itu/GreenField/repository/CommandesRepository.java
+++ src/main/java/itu/GreenField/repository/CommandesRepository.java
@@ -2,6 +2,9 @@ package itu.GreenField.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 
 import itu.GreenField.model.Commandes;
 import java.util.List;
@@ -12,5 +15,12 @@ public interface CommandesRepository extends JpaRepository<Commandes, Integer> {
     @Query(value = "SELECT * FROM commandes c " +
             "WHERE c.statutcommande != 'Paye' " +
             "AND c.statutcommande != 'Annule'", nativeQuery = true)
-    List<Commandes> findDispoCommandes();
+    public List<Commandes> findDispoCommandes();
+
+    @Query(value = "SELECT * FROM commandes c", nativeQuery = true)
+    public Page<Commandes> findAllPaginated(Pageable pageable);
+
+    @Query(value = "SELECT * FROM commandes c WHERE 1 = 1" +
+            "AND :statut IS NULL OR :statut = '' OR c.statutcommande = :statut", nativeQuery = true)
+    public Page<Commandes> findFilteredAndPaginated(@Param("statut") String statut, Pageable pageable);
 }
