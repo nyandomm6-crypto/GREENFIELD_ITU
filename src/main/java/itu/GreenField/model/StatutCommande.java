1d9f788533b9683c770bc002d1771e2320c38072
MAMINIAVO Grace Tanteraka <gracetanteraka@gmail.com>
2026-06-27 11:58:17 +0300

    [feat] add getAllStatutCommande method to retrieve all order statuses

--- src/main/java/itu/GreenField/model/StatutCommande.java
+++ src/main/java/itu/GreenField/model/StatutCommande.java
@@ -1,8 +1,15 @@
 package itu.GreenField.model;
 
+import java.util.List;
+
 public enum StatutCommande {
     Cree,
     En_cours,
     Paye,
-    Annule
+    Annule;
+
+
+    public static List<String> getAllStatutCommande() {
+        return List.of(Cree.name(), En_cours.name(), Paye.name(), Annule.name());
+    }
 }
