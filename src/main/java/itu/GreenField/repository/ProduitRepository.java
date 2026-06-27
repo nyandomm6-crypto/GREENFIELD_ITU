a8c5868ba9f0b991dd9329555f70f2d8c7f5adb7
tahinaRAJ <tahinaraj1235@gmail.com>
2026-06-27 08:38:48 +0300

    repository

--- src/main/java/itu/GreenField/repository/ProduitRepository.java
+++ src/main/java/itu/GreenField/repository/ProduitRepository.java
@@ -1,8 +1,15 @@
 package itu.GreenField.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import itu.GreenField.model.Produit;
 
 public interface ProduitRepository extends JpaRepository<Produit, Integer> {
+    List<Produit> findByCategorie_Id(Integer idCategorie);
+
+    List<Produit> findByNomContainingIgnoreCase(String motCle);
+
+    List<Produit> findByCategorie_IdAndNomContainingIgnoreCase(Integer idCategorie, String motCle);
 }
