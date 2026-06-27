90f567c12ececf935dcbe09223bbc406f664bd9f
MAMINIAVO Grace Tanteraka <gracetanteraka@gmail.com>
2026-06-27 12:02:55 +0300

    [feat] add FiltreNombreBackCommandeOption enum for filtering options in back office commandes

---
+++ src/main/java/itu/GreenField/filtre/FiltreNombreBackCommandeOption.java
@@ -0,0 +1,19 @@
+package itu.GreenField.filtre;
+
+public enum FiltreNombreBackCommandeOption {
+    ALL("", "Tous"),
+    QUANTITE_TOTALE("total_produits", "Quantité totale"),
+    VALEUR_TOTALE("total_general", "Montant total"),
+    FRAIS_LIVRAISON("frais_livraison", "Frais livraison");
+
+    private final String value;
+    private final String label;
+
+    FiltreNombreBackCommandeOption(String value, String label) {
+        this.value = value;
+        this.label = label;
+    }
+
+    public String getValue() { return value; }
+    public String getLabel() { return label; }
+}
