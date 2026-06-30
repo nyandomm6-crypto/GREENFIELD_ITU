94d4d3cf763ada6c0e9a39515d50cc1d9ffc0ded
RAKOTONIAINA Miantra Vahatrasoa <rakotoniainami5@gmail.com>
2026-06-30 14:52:08 +0300

    form controller

--- src/main/java/itu/GreenField/controller/PaiementController.java
+++ src/main/java/itu/GreenField/controller/PaiementController.java
@@ -1,14 +1,14 @@
 package itu.GreenField.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PaiementController {
 
     @GetMapping("/paiement/form/")
-    public String showForm() {
-
+    public String showForm(Model model) {
 
         return "paiement/form_paiement";
     }
