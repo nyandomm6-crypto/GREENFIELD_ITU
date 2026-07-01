56a9a875536ec69861eaa7dfe72ccfa2d2590443
TahinaRAJ <tahinaraj1235@gmail.com>
2026-07-01 20:51:20 +0300

    controller Faq

---
+++ src/main/java/itu/GreenField/controller/FaqController.java
@@ -0,0 +1,13 @@
+package itu.GreenField.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FaqController {
+
+    @GetMapping("/faq")
+    public String afficherFaq() {
+        return "front/faq/faq";
+    }
+}
