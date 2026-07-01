9df7600a3d8a0a9c820b5ac87679abdc7cb05bf5
nyandomm4 <nyandomm6@gmail.com>
2026-07-01 16:05:23 +0300

    d

--- src/main/java/itu/greenField/repository/VehiculeRepository.java
+++ src/main/java/itu/greenField/repository/VehiculeRepository.java
@@ -7,23 +7,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import itu.greenField.*;
 import itu.greenField.model.StatutVehicule;
 import itu.greenField.model.Vehicule;
 
 public interface VehiculeRepository extends JpaRepository<Vehicule, Integer> {
-        <<<<<<<HEAD:src/main/java/itu/greenField/repository/VehiculeRepository.java
-    public Vehicule getById(Integer id);=======
+    // II-Liste vehicule : Filtre par mot clés, date, status
+    @Query("SELECT v FROM Vehicule v WHERE " +
+            "(:motCle IS NULL OR LOWER(v.marque) LIKE LOWER(CONCAT('%', :motCle, '%')) " +
+            "OR LOWER(v.modele) LIKE LOWER(CONCAT('%', :motCle, '%')) " +
+            "OR LOWER(v.matricule) LIKE LOWER(CONCAT('%', :motCle, '%'))) " +
+            "AND (:date IS NULL OR v.date = :date) " +
+            "AND (:statut IS NULL OR v.statut = :statut)")
+    List<Vehicule> filtrerVehicules(@Param("motCle") String motCle,
+            @Param("date") LocalDate date,
+            @Param("statut") StatutVehicule statut);
 
-        // II-Liste vehicule : Filtre par mot clés, date, status
-        @Query("SELECT v FROM Vehicule v WHERE " +
-                        "(:motCle IS NULL OR LOWER(v.marque) LIKE LOWER(CONCAT('%', :motCle, '%')) " +
-                        "OR LOWER(v.modele) LIKE LOWER(CONCAT('%', :motCle, '%')) " +
-                        "OR LOWER(v.matricule) LIKE LOWER(CONCAT('%', :motCle, '%'))) " +
-                        "AND (:date IS NULL OR v.date = :date) " +
-                        "AND (:statut IS NULL OR v.statut = :statut)")
-        List<Vehicule> filtrerVehicules(@Param("motCle") String motCle,
-                        @Param("date") LocalDate date,
-                        @Param("statut") StatutVehicule statut);
-
-        >>>>>>>module-vehicule:src/main/java/itu/greenField/repository/VehiculeRepository.java
 }
