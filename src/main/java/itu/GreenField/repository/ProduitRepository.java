fd9ec6308425190fec18663bc38a65ece61fbbab
MAMINIAVO Grace Tanteraka <gracetanteraka@gmail.com>
2026-07-02 11:35:15 +0300

    [enhance] add findFirstByNom method to ProduitRepository for retrieving products by name

--- src/main/java/itu/GreenField/repository/ProduitRepository.java
+++ src/main/java/itu/GreenField/repository/ProduitRepository.java
@@ -1,8 +1,14 @@
 package itu.GreenField.repository;
 
+import java.lang.StackWalker.Option;
+import java.nio.file.OpenOption;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import itu.GreenField.model.Produit;
 
+import java.util.Optional;
+
 public interface ProduitRepository extends JpaRepository<Produit, Integer> {
+    public Optional<Produit> findFirstByNom(String nom);
 }
