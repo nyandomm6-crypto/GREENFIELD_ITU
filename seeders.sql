d04fca31d7057aa9890eb8aef21aa41e29fef3c9
MAMINIAVO Grace Tanteraka <gracetanteraka@gmail.com>
2026-07-02 11:54:27 +0300

    [enhance] update employee roles in seeders.sql for clarity and consistency

--- seeders.sql
+++ seeders.sql
@@ -7,41 +7,41 @@ INSERT INTO Employes (
     'IAH 23I Vontovorona', '034 55 123 45', 
     'grace.admin@greenfield.mg', 
     '$2a$10$vI8YV6A3p7yV0z4S6zX5eu2v1Vp4E2K/1Ew9fQ0wR3KzX5eu2v1Vp', -- "password"
-    'ADMIN', 'CTR-001', TRUE, '2026-01-10'
+    'Administrateur', 'CTR-001', TRUE, '2026-01-10'
 ),
 
--- 2. Un Responsable pour le Kiosque Nord
+-- 2. Un Manager pour le Kiosque Nord
 (
     'RABEMANANJARA', 'Sitraka', 
     'Analamahitsy, Antananarivo', '034 66 789 01', 
     'sitraka.rabe@greenfield.mg', 
     '$2a$10$vI8YV6A3p7yV0z4S6zX5eu2v1Vp4E2K/1Ew9fQ0wR3KzX5eu2v1Vp', -- "password"
-    'RESPONSABLE', 'BTQ-001', TRUE, '2026-02-15'
+    'Manager', 'BTQ-001', TRUE, '2026-02-15'
 ),
 
--- 3. Un Vendeur pour le Kiosque Sud
+-- 3. Un Caissier au Kiosque Sud
 (
     'RASOLO', 'Faly', 
     'Ankatso, Antananarivo', '033 77 888 99', 
     'faly.rasolo@greenfield.mg', 
     '$2a$10$vI8YV6A3p7yV0z4S6zX5eu2v1Vp4E2K/1Ew9fQ0wR3KzX5eu2v1Vp', -- "password"
-    'VENDEUR', 'BTQ-002', TRUE, '2026-03-01'
+    'Caissier', 'BTQ-002', TRUE, '2026-03-01'
 ),
 
--- 4. Un Vendeur pour le Kiosque Est
+-- 4. Un Livreur rattaché au Kiosque Est
 (
     'ANDRIANTOAVINA', 'Rova', 
     'Ambohipo, Antananarivo', '032 88 999 00', 
     'rova.andria@greenfield.mg', 
     '$2a$10$vI8YV6A3p7yV0z4S6zX5eu2v1Vp4E2K/1Ew9fQ0wR3KzX5eu2v1Vp', -- "password"
-    'VENDEUR', 'BTQ-003', TRUE, '2026-04-20'
+    'Livreur', 'BTQ-003', TRUE, '2026-04-20'
 ),
 
--- 5. Un ancien employé (Inactif) qui était au Kiosque Ouest
+-- 5. Un Employé standard au Kiosque Ouest
 (
     'RAKOTOMALALA', 'Jean', 
     '67 Ha, Antananarivo', '034 00 111 22', 
     'jean.rakoto@greenfield.mg', 
     '$2a$10$vI8YV6A3p7yV0z4S6zX5eu2v1Vp4E2K/1Ew9fQ0wR3KzX5eu2v1Vp', -- "password"
-    'VENDEUR', 'BTQ-004', FALSE, '2025-11-12'
+    'Employe', 'BTQ-004', TRUE, '2026-05-12'
 );
