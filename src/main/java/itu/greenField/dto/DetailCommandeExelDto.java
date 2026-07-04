01d37597e50fe81a237b11daab1dc598b8ec7392
MAMINIAVO Grace Tanteraka <gracetanteraka@gmail.com>
2026-07-04 17:53:44 +0300

    [feature] add DetailCommandeExelDto class for order details representation

---
+++ src/main/java/itu/greenField/dto/DetailCommandeExelDto.java
@@ -0,0 +1,34 @@
+package itu.greenField.dto;
+
+import itu.greenField.model.Produit;
+
+public class DetailCommandeExelDto {
+    private Integer numCommande;
+    private Produit produit;
+    private Integer quantite;
+
+    public Integer getNumCommande() {
+        return numCommande;
+    }
+
+    public void setNumCommande(Integer numCommande) {
+        this.numCommande = numCommande;
+    }
+
+    public Produit getProduit() {
+        return produit;
+    }
+
+    public void setProduit(Produit produit) {
+        this.produit = produit;
+    }
+
+    public Integer getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(Integer quantite) {
+        this.quantite = quantite;
+    }
+
+}
