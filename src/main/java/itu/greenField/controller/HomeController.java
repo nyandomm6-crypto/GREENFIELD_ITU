6b8dbe3dd9f8a2c77ff5722396f6799fd1601524
MAMINIAVO Grace Tanteraka <gracetanteraka@gmail.com>
2026-07-04 23:10:46 +0300

    [feature] update HomeController to serve dashboard view

--- src/main/java/itu/greenField/controller/HomeController.java
+++ src/main/java/itu/greenField/controller/HomeController.java
@@ -2,12 +2,14 @@ package itu.greenField.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String home() {
-        return "redirect:/pointdevente";
+    @GetMapping({"/", "/dashboard"})
+    public ModelAndView getDashboard() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        return modelAndView;
     }
 }
