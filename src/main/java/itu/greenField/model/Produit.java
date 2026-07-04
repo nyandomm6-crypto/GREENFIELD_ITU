a95035d196651263a4171e65d46fb840ef5fbf88
nyandomm4 <nyandomm6@gmail.com>
2026-07-04 12:24:14 +0300

    FIX

--- src/main/java/itu/greenField/model/Produit.java
+++ src/main/java/itu/greenField/model/Produit.java
@@ -3,6 +3,8 @@ package itu.greenField.model;
 import java.math.BigDecimal;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
